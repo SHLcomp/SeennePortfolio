c6bbc8f2af4277f8d4964b876efa62a302686209
SHLcomp <salibahe123@gmail.com>
2026-02-14 04:08:47 +0300

    added the demo links

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,14 +7,14 @@ const projects = [
     desc: "Modern responsive restaurant UI with interactive animations.",
     tech: ["React", "GSAP", "SCSS"],
     img: img1,
-    demo: "#",
+    demo: "https://seenne-cuisine.vercel.app/",
   },
   {
     title: "Consulting Website",
     desc: "Corporate consulting landing page with professional branding.",
     tech: ["React", "Vite", "SCSS"],
     img:img2,
-    demo: "#",
+    demo: "https://lou-2.vercel.app/",
   },
 ];
 
